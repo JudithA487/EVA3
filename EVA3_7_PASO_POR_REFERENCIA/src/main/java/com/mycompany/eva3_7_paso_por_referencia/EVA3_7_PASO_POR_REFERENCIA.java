79b0a8f7a80fd2d1d4c4aa33f5307f15f373660d
JudithA487 <143568356+JudithA487@users.noreply.github.com>
2023-11-27 21:08:15 -0600

    Add files via upload

---
+++ EVA3_7_PASO_POR_REFERENCIA/src/main/java/com/mycompany/eva3_7_paso_por_referencia/EVA3_7_PASO_POR_REFERENCIA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_7_paso_por_referencia;
+
+/**
+ *
+ * @author acost
+ */
+public class EVA3_7_PASO_POR_REFERENCIA {
+
+    public static void main(String[] args) {
+        int[] arreglo = new int[10];
+        System.out.println("El valor en arreglo en el main " + arreglo);
+        llenarArreglo(arreglo);
+        for (int i = 0; i < valores.length; i++) {
+            System.out.print("[" + arreglo[i] + "]");
+    }
+    
+    public static void llenarArreglo(int[] valores){
+        System.out.println("El valor del arreglo en llenarArreglo es " + arreglo);
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = int (int) (Math.random() * 100;
+        }
+    }
+}
